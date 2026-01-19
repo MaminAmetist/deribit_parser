2caf133695fef77e70a4eb55cc00baaecfc8ace5
Anna <anna.nezdulkina@mail.ru>
2026-01-19 22:04:00 +0700

    add test deribit

--- tests/test_service_deribit.py
+++ tests/test_service_deribit.py
@@ -5,25 +5,27 @@ import pytest
 from app.services.deribit import fetch_index_price
 
 
+class DummyResponse:
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        pass
+
+    async def json(self):
+        return {"result": {"index_price": 123.456}}
+
+    def raise_for_status(self):
+        return None
+
+
+class DummySession:
+    def get(self, *args, **kwargs):
+        return DummyResponse()
+
+
 @pytest.mark.asyncio
-async def test_fetch_index_price_success(monkeypatch):
-    class DummySession:
-        async def get(self, *args, **kwargs):
-            class Resp:
-                async def json(self):
-                    return {"result": {"index_price": 123.456}}
-
-                def raise_for_status(self):
-                    return None
-
-            return Resp()
-
+async def test_fetch_index_price_success():
     price = await fetch_index_price(DummySession(), "BTC")
-    assert isinstance(price, Decimal)
+
     assert price == Decimal("123.456")
-
-
-@pytest.mark.asyncio
-async def test_fetch_index_price_unsupported(monkeypatch):
-    with pytest.raises(ValueError):
-        await fetch_index_price(None, "BAD")
